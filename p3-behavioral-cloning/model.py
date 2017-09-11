6b8960e3bdb597104a5af74645ad43acf974ecf3
Junyi Yuan <yuanjunyi.mail@gmail.com>
2017-09-11 15:57:50 +0200

    Added 1 convolution layer to the model. Removed 2 fully connected layers. Added droput before the first fully connected layers. Replaced track1.mp4 and track2.mp4

--- p3-behavioral-cloning/model.py
+++ p3-behavioral-cloning/model.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 from keras.models import Sequential, load_model
-from keras.layers import Flatten, Dense, Lambda, Activation
+from keras.layers import Flatten, Dense, Lambda, Activation, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Cropping2D
 from keras.backend import get_value, set_value
 
@@ -64,8 +64,8 @@ def resize(image):
 def main(data_path, resume, epochs, learning_rate):
     samples = read_data(data_path)
     train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-    train_generator = generator(data_path, train_samples, batch_size=128)
-    validation_generator = generator(data_path, validation_samples, batch_size=128)
+    train_generator = generator(data_path, train_samples, batch_size=64)
+    validation_generator = generator(data_path, validation_samples, batch_size=64)
 
     model = None
     if resume:
@@ -73,28 +73,43 @@ def main(data_path, resume, epochs, learning_rate):
     else:
         model = Sequential()
         model.add(Lambda(resize, input_shape=(160,320,3)))
-        model.add(Cropping2D(cropping=((25,10), (0,0))))
+        model.add(Cropping2D(cropping=((20,10), (0,0))))
         model.add(Lambda(lambda x: x/255.0-0.5))
-        model.add(Conv2D(nb_filter=16, nb_row=5, nb_col=5, subsample=(2, 2), border_mode='valid'))
+
+        # Convolution
+        model.add(Conv2D(nb_filter=16, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='same'))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), border_mode='same'))
-        model.add(Conv2D(nb_filter=32, nb_row=5, nb_col=5, subsample=(2, 2), border_mode='valid'))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+
+        # Convolution
+        model.add(Conv2D(nb_filter=32, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='same'))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), border_mode='same'))
-        model.add(Conv2D(nb_filter=64, nb_row=5, nb_col=5, subsample=(2, 2), border_mode='valid'))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+
+        # Convolution
+        model.add(Conv2D(nb_filter=64, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='same'))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), border_mode='same'))
-        model.add(Conv2D(nb_filter=128, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='valid'))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+
+        # Convolution
+        model.add(Conv2D(nb_filter=128, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='same'))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), border_mode='same'))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+
+        # Convolution
+        model.add(Conv2D(nb_filter=128, nb_row=3, nb_col=3, subsample=(1, 1), border_mode='same'))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+
         model.add(Flatten())
-        model.add(Dense(1000))
-        model.add(Dense(500))
+        model.add(Dropout(0.5))
         model.add(Dense(100))
         model.add(Dense(50))
         model.add(Dense(10))
         model.add(Dense(1))
         model.compile(loss='mse', optimizer='adam')
+
+    print(model.summary())
 
     set_value(model.optimizer.lr, learning_rate)
     h = model.fit_generator(generator=train_generator, validation_data=validation_generator, samples_per_epoch=len(train_samples), nb_val_samples=len(validation_samples), nb_epoch=epochs)
@@ -110,7 +125,7 @@ def main(data_path, resume, epochs, learning_rate):
 
 
 if __name__ == '__main__':
-    data_path, resume, epochs, learning_rate = 'data', False, 20, 0.0001
+    data_path, resume, epochs, learning_rate = 'data', False, 10, 0.001
     if len(sys.argv) == 2:
         data_path = sys.argv[1]
     if len(sys.argv) == 5:
