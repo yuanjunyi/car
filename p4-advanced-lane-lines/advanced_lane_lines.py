ccada06caa3e2878e8b8432b33defb96d943be80
Junyi Yuan <yuanjunyi.mail@gmail.com>
2017-10-05 15:39:14 +0200

    Calculate curvature

--- p4-advanced-lane-lines/advanced_lane_lines.py
+++ p4-advanced-lane-lines/advanced_lane_lines.py
@@ -222,6 +222,22 @@ def find_lines(binary_warped):
     plt.xlim(0, 1280)
     plt.ylim(720, 0)
 
+    # Define conversions in x and y from pixels space to meters
+    ym_per_pix = 30 / 720
+    xm_per_pix = 3.7 / 700
+
+    # Fit new polynomials to x,y in world space
+    left_fit_cr = np.polyfit(lefty*ym_per_pix, leftx*xm_per_pix, 2)
+    right_fit_cr = np.polyfit(righty*ym_per_pix, rightx*xm_per_pix, 2)
+
+    # Calculate the new radii of curvature
+    y_eval = np.max(ploty)
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+
+    # Now our radius of curvature is in meters
+    print(left_curverad, 'm', right_curverad, 'm')
+
 
 def pipeline(image, detail):
     # Undistort image
