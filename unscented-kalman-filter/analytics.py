3ec78a8e0bf54bb2e0b3f6c0b32fb9025981432e
Junyi Yuan <yuanjunyi.mail@gmail.com>
2018-01-02 10:53:31 +0100

    Tune process noise std_yawdd_

--- unscented-kalman-filter/analytics.py
+++ unscented-kalman-filter/analytics.py
@@ -8,14 +8,16 @@ if __name__ == '__main__':
 		measurements = []
 		for line in lines:
 			words = line.split('\t')
-			vx, vy, yaw = [float(word) for word in words[-4:-1]]
-			measurements.append([vx, vy, yaw])
+			vx, vy, yaw, yawrate = [float(word) for word in words[-4:]]
+			measurements.append([vx, vy, yaw, yawrate])
 
 		delta_t = 0.05
 		acceleration_vx = [(j[0]-i[0]) / 0.05 for i, j in zip(measurements[:-1], measurements[1:])]
 		acceleration_vy = [(j[1]-i[1]) / 0.05 for i, j in zip(measurements[:-1], measurements[1:])]
 		acceleration_yaw = [(j[2]-i[2]) / 0.05 for i, j in zip(measurements[:-1], measurements[1:])]
+		acceleration_yawrate = [(j[3]-i[3]) / 0.05 for i, j in zip(measurements[:-1], measurements[1:])]
 		
 		print(np.std(acceleration_vx), np.max(acceleration_vx))
 		print(np.std(acceleration_vy), np.max(acceleration_vy))
 		print(np.std(acceleration_yaw), np.max(acceleration_yaw))
+		print(np.std(acceleration_yawrate), np.max(acceleration_yawrate))
