b6a8df795433f80c2925564346dccd9f7d95361d
Junyi Yuan <yuanjunyi.mail@gmail.com>
2018-01-17 17:38:44 +0100

    Tune parameter

--- pid-controller/src/main.cpp
+++ pid-controller/src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.05, 0.0006, 5);
+  pid.Init(0.2, 0, 8);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -59,8 +59,8 @@ int main()
           * another PID controller to control the speed!
           */
           pid.UpdateError(cte);
-          double steer = pid.TotalError();
-          double throttle = 0.3 - abs(cte) * 0.06;
+          const double steer = pid.TotalError();
+          const double throttle = 0.3 - abs(cte) * 0.06;
           
           // DEBUG
           std::cout << "CTE: " << cte
