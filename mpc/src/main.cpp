eae4f8027655cb6ce479376a6708b98ac24a1d35
Junyi Yuan <yuanjunyi.mail@gmail.com>
2018-01-26 12:05:59 +0100

    Fix compile error

--- mpc/src/main.cpp
+++ mpc/src/main.cpp
@@ -102,7 +102,8 @@ int main() {
           double px = j[1]["x"];
           double py = j[1]["y"];
           double psi = j[1]["psi"];
-          double v = j[1]["speed"] * 0.44704; // Convert from mph to m/s
+          double v = j[1]["speed"];
+          v *= 0.44704; // Convert from mph to m/s
 
           // Transform waypoints from map coordinate to car coordinate.
           vector<double> waypointx;
